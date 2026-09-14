ee2baa235694f65b4db644beedcc89c5ac4194ba
darshan <0daxx@proton.me>
2026-09-14 13:54:06 +0530

    added authentication context

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,17 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 import { Ionicons } from "@react-native-vector-icons/ionicons";
-
+import { useAuthStore } from "@/context/authContext";
 export default function RootLayout() {
-  const isLoggedIn = true;
+  const {isAuth , setIsAuth} = useAuthStore();
   // const isLoggedIn = false;
-
-  // TODO :  Replace with your actual authentication logic
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Protected guard={!isLoggedIn}>
-        <Stack.Screen name="(auth)" />
-      </Stack.Protected>
-      <Stack.Protected guard={isLoggedIn}>
-        <Stack.Screen name="(tabs)" />
-      </Stack.Protected>
-    </Stack>
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Protected guard={!isAuth}>
+        <Tabs.Screen name="(auth)" />
+      </Tabs.Protected>
+      <Tabs.Protected guard={isAuth}>
+        <Tabs.Screen name="(tabs)" />
+      </Tabs.Protected>
+    </Tabs>
   );
 }
